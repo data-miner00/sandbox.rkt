887d34dcb844ec5188f8657e27cb0a84dc737678
Shaun Chong <data-miner00@users.noreply.github.com>
2024-11-18 01:35:41 +0800

    feat: Added some functions

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/sandbox_rkt.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "A sandbox for learning Racket.")
 (define version "0.0")
 (define pkg-authors '(User))
 (define license '(Apache-2.0 OR MIT))
--- main.rkt
+++ main.rkt
@@ -7,6 +7,13 @@
   (check-equal? (+ 2 2) 4))
 
 (module+ main
+  (define (addition num1 num2)
+    (+ num1 num2))
+  (define increment
+    (lambda (arg)
+      (+ 1 arg))))
+
+(module+ main
   (require racket/cmdline)
   (define who (box "world"))
   (command-line
@@ -14,4 +21,10 @@
     #:once-each
     [("-n" "--name") name "Who to say hello to" (set-box! who name)]
     #:args ()
-    (printf "hello ~a~n" (unbox who))))
+
+    ;; Prints hello world or name
+    (printf "hello ~a~n" (unbox who))
+
+    (printf "5 + 3 is ~a~n" (addition 5 3))
+
+    (printf "5 increment is ~a~n" (increment 5))))
